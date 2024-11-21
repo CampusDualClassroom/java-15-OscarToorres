56bb53b3b6714d7a71aa58d58a0fa4b2c6c6c555
Oscar Torres Rodriguez <torresrdriguezoscar@gmail.com>
2024-11-21 11:27:40 +0100

    Ejercicio 15 completado

--- src/main/java/com/campusdual/classroom/FreshMerchandise.java
+++ src/main/java/com/campusdual/classroom/FreshMerchandise.java
@@ -5,4 +5,34 @@ import java.util.Date;
 
 public class FreshMerchandise extends Merchandise {
 
+    private Date expirationDate ;
+
+    public FreshMerchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf, int quantity, Date expirationDate) {
+        super(name, uniqueId, responsibleId, zone, area, shelf, quantity);
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public Object getSpecificData() {
+        StringBuilder sb = new StringBuilder();
+
+        return sb.append("Localizacion: " + responsibleId + "Data: " + getFormattedDate(getExpirationDate()));
+    }
+
+    public void printSpecificData() {
+        System.out.println(getSpecificData());
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getFormattedDate(Date expirationDate) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        return sdf.format(getExpirationDate());
+    }
 }
